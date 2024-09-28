d3a59059c9acaddba124d6a43a00b7ed5f31d52a
Michael Seelisch <info@michaelseelisch.de>
2024-09-28 23:37:38 +0200

    updating our directory page with directory component and category item component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const domNode = document.getElementById('root');
+const root = ReactDOM.createRoot(domNode);
+
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
