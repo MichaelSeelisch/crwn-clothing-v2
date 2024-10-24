38a329d393c7b77e77e0be6d4ebafd6b5ea23d67
Michael Seelisch <info@michaelseelisch.de>
2024-10-24 12:59:49 +0200

    created checkout page along with checkout items and checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,12 +2,19 @@ import './cart-dropdown.styles.scss';
 
 import { useContext } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
 	const { cartItems } = useContext(CartContext);
+	const navigate = useNavigate();
+
+	const gotToCheckoutHandler = () => {
+		navigate('/checkout');
+	}
 
 	return (
 		<div className='cart-dropdown-container'>
@@ -22,7 +29,9 @@ const CartDropdown = () => {
 				})}
 			</div>
 
-			<Button>GO TO CHECKOUT</Button>
+			<Button onClick={gotToCheckoutHandler}>
+				GO TO CHECKOUT
+			</Button>
 		</div>
 	);
 }
